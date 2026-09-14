56a3ad6858ee4b1c584a9fca36a0736a7ee61d5a
Matthias Leuffen <matthes@leuffen.de>
2026-09-14 04:43:57 +0200

    Require aliases for document images

--- src/Core/Document.php
+++ src/Core/Document.php
@@ -8,6 +8,8 @@ abstract class Document
 {
     private string $markdown = '';
     private ?string $html = null;
+    /** @var array<string, ImageSource> */
+    private array $images = [];
 
     final public function markdown(string $markdown): static
     {
@@ -23,8 +25,30 @@ abstract class Document
         return $this;
     }
 
+    final public function image(string $alias, ImageSource $source): static
+    {
+        $this->images[$alias] = $source;
+        return $this;
+    }
+
+    final public function imageFile(string $alias, string $filename, string $mimeType): static
+    {
+        return $this->image($alias, ImageSource::file($filename, $mimeType));
+    }
+
+    final public function imageBytes(string $alias, string $bytes, string $mimeType): static
+    {
+        return $this->image($alias, ImageSource::bytes($bytes, $mimeType));
+    }
+
+    final public function imageResolver(string $alias, callable $resolver, string $mimeType): static
+    {
+        return $this->image($alias, ImageSource::resolver($resolver, $mimeType));
+    }
+
     final public function getMarkdown(): string { return $this->markdown; }
     final public function getHtml(): ?string { return $this->html; }
+    final public function getImages(): array { return $this->images; }
 
     abstract public function template(): string;
     abstract public function variables(): array;
