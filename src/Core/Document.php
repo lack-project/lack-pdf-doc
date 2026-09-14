582514050a39475385c8d927137ac1289246c560
Matthias Leuffen <matthes@leuffen.de>
2026-09-14 04:48:35 +0200

    Require aliases for document resources

--- src/Core/Document.php
+++ src/Core/Document.php
@@ -8,8 +8,12 @@ abstract class Document
 {
     private string $markdown = '';
     private ?string $html = null;
+
     /** @var array<string, ImageSource> */
     private array $images = [];
+
+    /** @var array<string, FontSource> */
+    private array $fonts = [];
 
     final public function markdown(string $markdown): static
     {
@@ -31,11 +35,6 @@ abstract class Document
         return $this;
     }
 
-    final public function imageFile(string $alias, string $filename, string $mimeType): static
-    {
-        return $this->image($alias, ImageSource::file($filename, $mimeType));
-    }
-
     final public function imageBytes(string $alias, string $bytes, string $mimeType): static
     {
         return $this->image($alias, ImageSource::bytes($bytes, $mimeType));
@@ -46,9 +45,16 @@ abstract class Document
         return $this->image($alias, ImageSource::resolver($resolver, $mimeType));
     }
 
+    final public function font(string $alias, FontSource $source): static
+    {
+        $this->fonts[$alias] = $source;
+        return $this;
+    }
+
     final public function getMarkdown(): string { return $this->markdown; }
     final public function getHtml(): ?string { return $this->html; }
     final public function getImages(): array { return $this->images; }
+    final public function getFonts(): array { return $this->fonts; }
 
     abstract public function template(): string;
     abstract public function variables(): array;
